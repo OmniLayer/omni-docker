fe0fddd2bb1eb3a97244f1da1de2ff7dcfcd427f
Sean Gilligan <sean@msgilligan.com>
2021-03-06 15:01:19 -0800

    regtest script: Add 2 0MQ topics on 2 TCP ports

--- run-omni-regtest-docker.sh
+++ run-omni-regtest-docker.sh
@@ -2,6 +2,8 @@
 docker run --rm --name omnicored-regtest -it \
   -p 18443:18443 \
   -p 18444:18444 \
+  -p 38443:38443 \
+  -p 38444:38444 \
   omnilayer/omnicored:0.9.0 \
   -printtoconsole \
   -debug=1 \
@@ -17,6 +19,8 @@ docker run --rm --name omnicored-regtest -it \
   -paytxfee=0.00001 \
   -minrelaytxfee=0.00001 \
   -listenonion=false \
+  -zmqpubrawblock=tcp://0.0.0.0:38443 \
+  -zmqpubrawtx=tcp://0.0.0.0:38444 \
   -rpcallowip=172.17.0.0/16 \
   -rpcbind=0.0.0.0 \
   -rpcuser=bitcoinrpc \
