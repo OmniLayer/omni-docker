9ab63d29b6b85907dc1ecc9b2d8db61e03bb8144
Sean Gilligan <sean@msgilligan.com>
2021-03-15 17:40:03 -0700

    1st cut at Docker Compose with override files, etc.

---
+++ compose-omniproxy/run-compose.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# Startup script to launch OmniProxy and Omni Core via Docker Compose.
+#
+
+#
+# Parse parameters to figure out which network to run
+#
+
+if [[ -z "$1" ]] ; then
+    echo "Error: must specify network"
+    echo "Usage: run-compose.sh [mainnet|testnet|regtest]"
+    exit 1
+elif [[ "$1" == "mainnet" ]] ; then
+    OVERRIDE=docker-compose.mainnet.yml
+elif [[ "$1" == "testnet" ]] ; then
+    OVERRIDE=docker-compose.testnet.yml
+elif [[ "$1" == "regtest" ]] ; then
+    OVERRIDE=docker-compose.override.yml
+else
+    echo "Error: invalid network parameter"
+    echo "Usage: run-compose.sh [mainnet|testnet|regtest]"
+    exit 1
+fi
+
+#
+# Set user to `testuser` and generate a random password
+#
+
+gen_password () {
+    chars=abcdefghijklmnipqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    pass=""
+    for i in {1..20} ; do
+        pass="$pass${chars:RANDOM%${#chars}:1}"
+    done
+    echo "$pass"
+}
+
+export RPCUSER="testuser"
+export RPCPASSWORD="$(gen_password)"
+
+#
+# Run `docker-compose up` with the correct "override" file for the selected network,
+# and using `sample.env` for setting the remaining environment variables.
+#
+
+docker-compose --env-file sample.env -f docker-compose.yml -f $OVERRIDE up
+
