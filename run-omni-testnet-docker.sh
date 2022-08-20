5680666d4a402d166db6c52fb1be550b9bcd0e89
Sean Gilligan <sean@msgilligan.com>
2022-08-20 09:40:22 -0700

    run-omni-testnet-docker.sh: read majority of config options from bitcoin.conf

--- run-omni-testnet-docker.sh
+++ run-omni-testnet-docker.sh
@@ -5,21 +5,5 @@ docker run --rm --name omnicored-testnet -v ${OMNI_SHARED_DATADIR}:/home/bitcoin
   -p 38332:38332 \
   -p 38333:38333 \
   omnilayer/omnicored:0.11.0 \
-  -printtoconsole \
-  -debug=1 \
   -testnet \
-  -txindex=1 \
-  -experimental-btc-balances=1 \
-  -peerbloomfilters \
-  -addresstype=legacy \
-  -limitancestorcount=750 \
-  -limitdescendantcount=750 \
-  -listenonion=false \
-  -zmqpubrawblock=tcp://0.0.0.0:38332 \
-  -zmqpubhashblock=tcp://0.0.0.0:38332 \
-  -zmqpubrawtx=tcp://0.0.0.0:38333 \
-  -zmqpubhashtx=tcp://0.0.0.0:38333 \
-  -rpcallowip=172.17.0.0/16 \
-  -rpcbind=0.0.0.0 \
-  -rpcuser=bitcoinrpc \
-  -rpcpassword=pass
+  -rpcallowip=172.17.0.0/16
