225b14b82297f0c6f64132bcfc045eba3aea3a1f
Sean Gilligan <sean@msgilligan.com>
2022-10-27 13:08:05 -0700

    docker run shell-scripts: use :latest not :0.11.0

--- run-omni-help-docker.sh
+++ run-omni-help-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker run --rm --name omnicored-help -it \
-  omnilayer/omnicored:0.11.0 \
+  omnilayer/omnicored:latest \
   -?
--- run-omni-testnet-docker.sh
+++ run-omni-testnet-docker.sh
@@ -4,6 +4,6 @@ docker run --rm --name omnicored-testnet -v ${OMNI_SHARED_DATADIR}:/home/bitcoin
   -p 18333:18333 \
   -p 38332:38332 \
   -p 38333:38333 \
-  omnilayer/omnicored:0.11.0 \
+  omnilayer/omnicored:latest \
   -testnet \
   -rpcallowip=172.17.0.0/16
